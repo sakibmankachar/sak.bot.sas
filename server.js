429fef36013c9ed311d7797da777eff4fb8ee3f9
Glitch (unmarred-kiwi-kite) <none>
2021-02-21 08:45:53 +0000

    👂🌔 Checkpoint ./server.js:27141663/97436

--- server.js
+++ server.js
@@ -251,3 +251,96 @@ $onlyPerms[manageserver;You need manage server permissions to set this!]
 $color[fffff1]
 $addTimestamp`
 });
+
+bot.command({
+  name: "setrank",
+  usage: "setrank <channel>",
+  description: "settings the levelup channel",
+  code: `$description[Rank channel has been set up to <#$mentionedChannels[1;yes]>]
+$color[01ff00]
+$setServerVar[rch;$mentionedChannels[1;yes]]
+$setServerVar[rsystem;1]
+$onlyBotPerms[mentioneveryone;{description:I dont have permission \`MENTION_EVERYONE\`}{color:ff2050}]
+$onlyPerms[manageserver;{description:You need \`MANAGE_SERVER\` permission}{color:ff2050}]
+$cooldown[5s;Please wait **%time%**]`
+});
+
+bot.command({
+  name: "resetrank",
+  usage: "resetrank",
+  description: "reset the levelup channel",
+  code: `$description[Rank channel has been set up to <#$mentionedChannels[1;yes]>]
+$color[01ff00]
+$setServerVar[rch;]
+$setServerVar[rmsg;$getVar[rmsg]]
+$setServerVar[rsystem;0]
+$onlyIf[$getServerVar[rsystem]>=1;{description:Leveling system is __disabled__ on this server}{color:ff2050}]
+$onlyBotPerms[mentioneveryone;{description:I dont have permission \`MENTION_EVERYONE\`}{color:ff2050}]
+$onlyPerms[manageserver;{description:You need \`MANAGE_SERVER\` permission}{color:ff2050}]
+$cooldown[5s;Please wait **%time%**]`
+});
+
+bot.command({
+  name: "$alwaysExecute",
+  code: `$useChannel[$getServerVar[rch]]
+$replaceText[$replaceText[$replaceText[$replaceText[$getServerVar[rmsg];{user.tag};$userTag];{user.mention};<@$authorID>];{level};$getUserVar[lvl]];{exp};$getUserVar[exp]]
+$setUserVar[lvl;$sum[$getUserVar[lvl];1]]
+$setUserVar[rexp;$multi[$getUserVar[rexp];2]]
+$onlyIf[$getUserVar[exp]>=$getUserVar[rexp];]
+$onlyForServers[$guildID;]`
+});
+
+bot.command({
+  name: "$alwaysExecute",
+  code: `$setUserVar[exp;$sum[$getUserVar[exp];$random[1;4]]]
+$onlyIf[$getServerVar[rsystem]>=1;]
+$onlyForServers[$guildID;]`
+});
+
+bot.awaitedCommand({
+  name: "errorrank",
+  code: `$setServerVar[rch;]
+$onlyForServers[$guildID;]`
+});
+
+bot.command({
+  name: "setrankmsg",
+  usage: "setrankmsg <message>",
+  description: "message for the leveled up",
+  code: `$description[You have been setted the message to:
+\`$message\`]
+$color[01ff00]
+$setServerVar[rmsg;$message]
+$onlyIf[$message!=;You can also use this variables:
+\`\`\`
+{user.tag} = $userTag
+{user.mention} = <@$authorID>
+{level} = 1
+{exp} = 25
+\`\`\`
+Current msg is:
+\`$getServerVar[rmsg]\`]
+$onlyBotPerms[mentioneveryone;managemessages;{description:I need permission \`MANAGE_MESSAGES\`/\`MENTION_EVERYONE\`}{color:ff2050}]
+$onlyPerms[manageserver;{description:You need \`MANAGE_SERVER\` permission}{color:ff2050}]
+$cooldown[5s;Please wait **%time%**]`
+});
+
+bot.command({
+  name: "rank",
+  aliases: ["level"],
+  usage: "rank (user)",
+  description: "see the current level and exp",
+  code: `$image[https://vacefron.nl/api/rankcard?username=$replaceText[$username[$mentioned[1;yes]]; ;+;-1]&avatar=$userAvatar[$mentioned[1;yes]]?size=4096&level=$getUserVar[lvl;$mentioned[1;yes]]&rank=&currentxp=$getUserVar[exp;$mentioned[1;yes]]&nextlevelxp=$getUserVar[rexp;$mentioned[1;yes]]&previouslevelxp=0&custombg=https://cdn.discordapp.com/attachments/793071150614970388/794565647760752650/20210101_205624.jpg&xpcolor=ffffff&isboosting=true]
+$onlyIf[$getServerVar[rsystem]>=1;{description:Leveling system is __disabled__}{color:ff2050}]
+$cooldown[5s;]`
+});
+
+bot.variables({
+  prefix: "s.",
+  rch: "",
+  rmsg: "Congrats {user.tag}🎉, you leveled up to level {level}",
+  lvl: "0",
+  exp: "0",
+  rexp: "40",
+  rsystem: "0"
+});

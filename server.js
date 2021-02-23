a868d2f6ddafa4af2dd222cb1a8f13effe4ec525
Glitch (unmarred-kiwi-kite) <none>
2021-02-23 10:25:05 +0000

    🐏👣 Checkpoint ./server.js:27141663/64022

--- server.js
+++ server.js
@@ -602,18 +602,19 @@ $author[$username;$authorAvatar]`
 });
 
 bot.command({
+  name: "invite",
 
-name: "invite",
-
-code: `$title[INVITE]
+  code: `$title[INVITE]
 
 $description[Want To Invite Me In Your Server?
 
-Here's my invite link[Click Here](https://discord.com/oauth2/authorize?client_id=804720808182022245&scope=bot&permissions=2080898175) 
+Here's my invite link 
+https://discord.com/oauth2/authorize?client_id=804720808182022245&scope=bot&permissions=2080898175
 
 If you want Support Then Join My Official Support Server
 
-Here my Official Support Server Join Link [Click Here](https://discord.gg/96BqdnWvh8) ]
+Here my Official Support Server Join Link 
+https://discord.gg/96BqdnWvh8 ]
 
 $footer[Sak-bot]`
-  })
+});

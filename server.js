9f2894d60a9e28ca048e6aac6be634de0e2c56a3
Glitch (a-sak-bot) <none>
2021-02-24 16:34:14 +0000

    🎂🎗 Checkpoint ./server.js:27141663/2

--- server.js
+++ server.js
@@ -226,7 +226,7 @@ $addTimestamp`
 
 bot.command({
   name: "setleave",
-  code: `$setServerVar[goodbye;$mentionedChannels[1:yes]]
+  code: `$setServerVar[goodbye;$mentionedChannels[1;yes]]
 $description[The channel <#$mentionedChannels[1]> was setted as goodbye channel, i will say goodbye in that channel to the users that leave]
 $footer[Setted by $username[$authorID]#$discriminator[$authorID]]
 $onlyPerms[manageserver;You need manage server permissions to set this!]
